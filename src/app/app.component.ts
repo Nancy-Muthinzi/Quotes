c87311cc8c4a7050e62c50ab2f9876480b90d788
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-08-10 11:13:03 +0300

    create quote property with a string array, initialize with quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import {Quote} from './quote'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Quotes';
+  quotes = [
+    new Quote(1, 'You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.' )
+  ]
+
 }
